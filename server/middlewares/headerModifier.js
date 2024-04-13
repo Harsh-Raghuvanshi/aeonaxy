6cfb5a3001a703f715ec90d139ab2c2b68351457
Harsh Raghuvanshi <harshrghvnsh@gmail.com>
2024-04-13 14:47:05 +0530

    Fifth commit

--- server/middlewares/headerModifier.js
+++ server/middlewares/headerModifier.js
@@ -1,15 +1,13 @@
-function responseHeaderModifier() {
-    return (req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Add PUT method
-      next();
-    };
-  }
-  
-module.exports={
-    responseHeaderModifier,
+function responseHeaderModifier(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Add PUT method
+  next();
 }
+
+module.exports = {
+  responseHeaderModifier: responseHeaderModifier // Export the function directly
+};
